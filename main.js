b16875dc7075697e4af90c71155812fcbaa7b06e
dblinkhorn <dblinkhorn@gmail.com>
2021-03-14 23:36:29 -0700

    added game() function and while loop to enable 5 rounds of play. also added declareWinner() function

--- main.js
+++ main.js
@@ -10,54 +10,87 @@ function computerPlay() {
     computerSelection = plays[Math.floor(Math.random() * plays.length)];
 }
 
+
 function showScore(playerScore, computerScore) {
     alert(`The score is now:
+
         Player: ${playerScore}
-        Computer: ${computerScore}`);
+        Computer: ${computerScore}
+        Round #: ${roundsPlayed}`);
 }
 
-function playRound(playerSelection, computerSelection) {
-    if (playerSelection === "rock" && computerSelection === "paper") {
-        computerScore += 1;
-        alert(`You lost! Paper beats Rock.`);
-    } else if (playerSelection === "paper" && computerSelection === "scissors") {
-        computerScore += 1;
-        alert(`You lost! Scissors beats Paper.`)
-    } else if (playerSelection === "scissors" && computerSelection === "rock") {
-        computerScore += 1;
-        alert(`You lost! Rock beats Scissors.`)
-    } else if (playerSelection === "rock" && computerSelection === "scissors") {
-        playerScore += 1;
-        alert(`You won! Rock beats Scissors.`)
-    } else if (playerSelection === "paper" && computerSelection === "rock") {
-        playerScore += 1;
-        alert(`You won! Paper beats Rock.`)
-    } else if (playerSelection === "scissors" && computerSelection === "paper") {
-        playerScore += 1;
-        alert(`You won! Scissors beats Paper.`)
+function declareWinner(playerScore, computerScore) {
+    if (playerScore > computerScore) {
+        alert(`Congratulations, you won!
+        
+        The final score was:
+        
+        Player: ${playerScore}
+        Computer: ${computerScore}`);
     } else {
-        alert("It's a tie! Let's play again...");
+        alert(`Sorry, you lost!
+        
+        The final score was:
+        
+        Player: ${playerScore}
+        Computer: ${computerScore}`);
     }
-    roundsPlayed += 1;
 }
 
 const plays = ["rock", "paper", "scissors"];
 
-let playerSelection = prompt(`Choose your weapon!: 
+let computerSelection;
+
+let computerScore = 0;
+let playerScore = 0;
+
+let roundsPlayed = 0;
+
+
+function game() {
+    while (roundsPlayed <= 5) {
+        function playRound(playerSelection, computerSelection) {
+            if (playerSelection === "rock" && computerSelection === "paper") {
+                computerScore += 1;
+                alert(`You lost! Paper beats Rock.`);
+            } else if (playerSelection === "paper" && computerSelection === "scissors") {
+                computerScore += 1;
+                alert(`You lost! Scissors beats Paper.`);
+            } else if (playerSelection === "scissors" && computerSelection === "rock") {
+                computerScore += 1;
+                alert(`You lost! Rock beats Scissors.`);
+            } else if (playerSelection === "rock" && computerSelection === "scissors") {
+                playerScore += 1;
+                alert(`You won! Rock beats Scissors.`);
+            } else if (playerSelection === "paper" && computerSelection === "rock") {
+                playerScore += 1;
+                alert(`You won! Paper beats Rock.`);
+            } else if (playerSelection === "scissors" && computerSelection === "paper") {
+                playerScore += 1;
+                alert(`You won! Scissors beats Paper.`);
+            } else {
+                alert("It's a tie! Let's play again...");
+                roundsPlayed -= 1;
+            }
+        }
+
+        let playerSelection = prompt(`Choose your weapon!: 
                         Rock
                         Paper
                         Scissors
                         
                         (Enter an item above)`);
 
-playerSelection = playerSelection.toLowerCase();
+        playerSelection = playerSelection.toLowerCase();
 
-let computerSelection;
-computerPlay();
+        computerPlay();
 
-let roundsPlayed = 0;
-let computerScore = 0;
-let playerScore = 0;
+        roundsPlayed += 1;
+        playRound(playerSelection, computerSelection);
+        showScore(playerScore, computerScore);
+    }
 
-playRound(playerSelection, computerSelection);
-showScore(playerScore, computerScore);
+    declareWinner(playerScore, computerScore);
+}
+
+game();
