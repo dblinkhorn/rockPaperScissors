ae3306dcbb7cdcc82301cfbc364d0178c3e441dd
dblinkhorn <dblinkhorn@gmail.com>
2021-03-15 18:02:04 -0700

    added input validation while loop

--- main.js
+++ main.js
@@ -6,11 +6,13 @@
 // Asks the player to play five rounds and keeps score, declaring the winner at the end
 
 
+// function to select computer's play
 function computerPlay() {
     computerSelection = plays[Math.floor(Math.random() * plays.length)];
 }
 
 
+// function to display the score to avoid repeating code in game() logic
 function showScore(playerScore, computerScore) {
     alert(`The score is now:
 
@@ -19,6 +21,8 @@ function showScore(playerScore, computerScore) {
         Round #: ${roundsPlayed}`);
 }
 
+
+// function which declares win or loss condition
 function declareWinner(playerScore, computerScore) {
     if (playerScore > computerScore) {
         alert(`Congratulations, you won!
@@ -37,18 +41,24 @@ function declareWinner(playerScore, computerScore) {
     }
 }
 
+// possible plays
 const plays = ["rock", "paper", "scissors"];
 
+// declares variable to store computer's play
 let computerSelection;
+let playerSelection;
 
+// sets scores to 0 to start, keeps track of score which changes each round in game() function
 let computerScore = 0;
 let playerScore = 0;
 
+// variable keeps track of # of played rounds
 let roundsPlayed = 0;
 
-
+// main game function, defines win/loss conditions
 function game() {
     while (roundsPlayed <= 5) {
+        // takes two arguments, player's selection and computer's selection and compares below
         function playRound(playerSelection, computerSelection) {
             if (playerSelection === "rock" && computerSelection === "paper") {
                 computerScore += 1;
@@ -69,19 +79,33 @@ function game() {
                 playerScore += 1;
                 alert(`You won! Scissors beats Paper.`);
             } else {
-                alert("It's a tie! Let's play again...");
+                alert("It's a tie! Let's play again.");
                 roundsPlayed -= 1;
             }
         }
+        
+        // prompts player to choose their play and stores in the variable
+        playerSelection = prompt(`Choose your weapon!:
+        Rock
+        Paper
+        Scissors
+            
+        (Enter an item above)`);
 
-        let playerSelection = prompt(`Choose your weapon!: 
-                        Rock
-                        Paper
-                        Scissors
-                        
-                        (Enter an item above)`);
-
+        // changes player input to all lower-case
         playerSelection = playerSelection.toLowerCase();
+
+        // while loop checks for valid user input and prompts until valid
+        while (playerSelection != "rock" && playerSelection != "paper" && playerSelection !="scissors") {
+            alert("You must select a valid weapon. Please try again.")
+
+            playerSelection = prompt(`Choose your weapon!:
+            Rock
+            Paper
+            Scissors
+            
+            (Enter an item above)`);
+        }
 
         computerPlay();
 
