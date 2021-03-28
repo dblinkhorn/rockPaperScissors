9a63237323fb5b8fe18a4e05b172fa2a38c24b83
dblinkhorn <dblinkhorn@gmail.com>
2021-03-27 23:44:08 -0700

    removed footer and made entire player selection divs clickable rather than just image.

--- rps.js
+++ rps.js
@@ -5,83 +5,70 @@
 // Script will determine winner or a tie and notify the player
 // Asks the player to play five rounds and keeps score, declaring the winner at the end
 
-
-// function to display the score to avoid repeating code in game() logic
-function showScore(playerScore, computerScore) {
-    alert(`The score is now:
-
-        Player: ${playerScore}
-        Computer: ${computerScore}
-        Round #: ${roundsPlayed}`);
-}
-
-
-// function which declares win or loss condition
-function declareWinner(playerScore, computerScore) {
-    if (playerScore > computerScore) {
-        alert(`Congratulations, you won!
-        
-        The final score was:
-        
-        Player: ${playerScore}
-        Computer: ${computerScore}`);
-    } else {
-        alert(`Sorry, you lost!
-        
-        The final score was:
-        
-        Player: ${playerScore}
-        Computer: ${computerScore}`);
-    }
-}
-
-// sets scores to 0 to start, keeps track of score which changes each round in game() function
+// sets scores to 0 to start, keeps track of score which changes each round in playRound function
 let computerScore = 0;
 let playerScore = 0;
 
-
+// function that determines outcome of the round
 function playRound(playerSelection) {
     // function to select computer's play
     function computerPlay() {
+    // variable stores possible plays
     const plays = ["rock", "paper", "scissors"];
+    // variable stores randomly selected item from plays above
     computerSelection = plays[Math.floor(Math.random() * 3)];
     return computerSelection;
     }
 
+    // set the variable to the returned play value
     computerSelection = computerPlay();
 
-    console.log(playerSelection);
-
-    console.log(computerSelection);
-
+    // main game logic, determines winner of round based on selections
     if (playerSelection === "rock" && computerSelection === "paper") {
+        // award point to score
         computerScore += 1;
-        document.getElementById("status").innerHTML = "<p>You lost! Paper beats rock!</p>"
+        // update status div with result of the round
+        document.getElementById("status").innerHTML = "<p>The computer chose paper.</p><p><br>You lost... Paper beats rock.</p>";
+        // update scores
+        document.getElementById("computer-score").innerHTML = `<br>${computerScore}`;
     } else if (playerSelection === "paper" && computerSelection === "scissors") {
         computerScore += 1;
-        document.getElementById("status").innerHTML = "<p>You lost! Scissors beats paper!</p>"
+        document.getElementById("status").innerHTML = "<p>The computer chose scissors.</p><p><br>You lost... Scissors beats paper.</p>";
+        document.getElementById("computer-score").innerHTML = `<br>${computerScore}`;
     } else if (playerSelection === "scissors" && computerSelection === "rock") {
         computerScore += 1;
-        document.getElementById("status").innerHTML = "<p>You lost! Rock beats scissors!</p>"
+        document.getElementById("status").innerHTML = "<p>The computer chose rock.</p><p><br>You lost... Rock beats scissors.</p>";
+        document.getElementById("computer-score").innerHTML = `<br>${computerScore}`;
     } else if (playerSelection === "rock" && computerSelection === "scissors") {
         playerScore += 1;
-        document.getElementById("status").innerHTML = "<p>You won! Rock beats scissors!</p>"
+        document.getElementById("status").innerHTML = "<p>The computer chose scissors.</p><p><br>You won! Rock beats scissors.</p>";
+        document.getElementById("player-score").innerHTML = `<br>${playerScore}`;
     } else if (playerSelection === "paper" && computerSelection === "rock") {
         playerScore += 1;
-        document.getElementById("status").innerHTML = "<p>You won! Paper beats rock!</p>"
+        document.getElementById("status").innerHTML = "<p>The computer chose rock.</p><p><br>You won! Paper beats rock.</p>";
+        document.getElementById("player-score").innerHTML = `<br>${playerScore}`;
     } else if (playerSelection === "scissors" && computerSelection === "paper") {
         playerScore += 1;
-        document.getElementById("status").innerHTML = "<p>You won! Scissors beats paper!</p>"
+        document.getElementById("status").innerHTML = "<p>The computer chose paper.</p><p><br>You won! Scissors beats paper.</p>";
+        document.getElementById("player-score").innerHTML = `<br>${playerScore}`;
     } else {
-        document.getElementById("status").innerHTML = "<p>It's a tie! Let's play again.</p>"
+        // let player know if it's a tie. score doesn't change
+        document.getElementById("status").innerHTML = `<p>The computer chose ${computerSelection}.</p><p><br>It's a tie! Let's play again.</p>`;
     }
+
+    
 }
 
+// define variable to store player's selection
 let playerSelection;
 
+// sets variable to the html element with id="rock"
 let rock = document.getElementById("rock");
+// if rock is clicked
 rock.addEventListener("click", function () {
+    // set variable to rock
     playerSelection = "rock";
+    // run function with "rock" as player's selection
     playRound(playerSelection);
 });
 
