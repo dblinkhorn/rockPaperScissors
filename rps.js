e446cf873b89fe64f53f02a8a084c57e0b28c5e8
dblinkhorn <dblinkhorn@gmail.com>
2021-03-28 20:30:57 -0700

    got rid of typewriter functionality for now. Changed all status alerts to use substitutions.

--- rps.js
+++ rps.js
@@ -17,20 +17,20 @@ let rock = document.getElementById("rock");
 // if rock element is clicked
 rock.addEventListener("click", function () {
     // set variable to rock
-    playerSelection = "rock";
+    playerSelection = "ROCK";
     // run function with "rock" as player's selection
     playRound(playerSelection);
 });
 
 let paper = document.getElementById("paper");
 paper.addEventListener("click", function () {
-    playerSelection = "paper";
+    playerSelection = "PAPER";
     playRound(playerSelection);
 });
 
 let scissors = document.getElementById("scissors");
 scissors.addEventListener("click", function () {
-    playerSelection = "scissors";
+    playerSelection = "SCISSORS";
     playRound(playerSelection);
 });
 
@@ -38,23 +38,10 @@ scissors.addEventListener("click", function () {
 // function that determines outcome of the round
 function playRound(playerSelection) {
 
-    // typewriter effect for round result announcement
-    var i = 0;
-    let txt;
-    var speed = 20; // The speed/duration of the effect in milliseconds
-
-    function typeWriter() {
-        if (i < txt.length) {
-            document.getElementById("status").innerHTML += txt.charAt(i);
-            i++;
-            setTimeout(typeWriter, speed);
-        }
-    }
-
     // function to select computer's play
     function computerPlay() {
     // variable stores possible plays
-    const plays = ["rock", "paper", "scissors"];
+    const plays = ["ROCK", "PAPER", "SCISSORS"];
     // variable stores randomly selected item from plays above
     computerSelection = plays[Math.floor(Math.random() * 3)];
     return computerSelection;
@@ -65,38 +52,35 @@ function playRound(playerSelection) {
     console.log(computerSelection);
 
     // main game logic, determines winner of round based on selections
-    if (playerSelection === "rock" && computerSelection === "paper") {
+    if (playerSelection === "ROCK" && computerSelection === "PAPER") {
         // award point to score
         computerScore += 1;
         // update status div with result of the round
-        txt = "The computer chose paper.\r\nYou lost... Paper beats rock.";
-        document.getElementById("status").innerHTML = typeWriter(txt);
-        txt = "\r\nYou lost... Paper beats rock.";
-        document.getElementById("status").innerHTML = typeWriter(txt);
+        document.getElementById("status").innerHTML = `<p>The computer chose ${computerSelection}.<br><br><br>You lost... ${computerSelection} beats ${playerSelection}.<br><br><br>Better luck next round!</p>`;
         // update score
         document.getElementById("computer-score").innerHTML = `<br>${computerScore}`;
-    } else if (playerSelection === "paper" && computerSelection === "scissors") {
+    } else if (playerSelection === "PAPER" && computerSelection === "SCISSORS") {
         computerScore += 1;
-        document.getElementById("status").innerHTML = "<p>The computer chose scissors.</p><p><br>You lost... Scissors beats paper.</p>";
+        document.getElementById("status").innerHTML = `<p>The computer chose ${computerSelection}.<br><br><br>You lost... ${computerSelection} beats ${playerSelection}.<br><br><br>Better luck next round!</p>`;
         document.getElementById("computer-score").innerHTML = `<br>${computerScore}`;
-    } else if (playerSelection === "scissors" && computerSelection === "rock") {
+    } else if (playerSelection === "SCISSORS" && computerSelection === "ROCK") {
         computerScore += 1;
-        document.getElementById("status").innerHTML = "<p>The computer chose rock.</p><p><br>You lost... Rock beats scissors.</p>";
+        document.getElementById("status").innerHTML = `<p>The computer chose ${computerSelection}.<br><br><br>You lost... ${computerSelection} beats ${playerSelection}.<br><br><br>Better luck next round!</p>`;
         document.getElementById("computer-score").innerHTML = `<br>${computerScore}`;
-    } else if (playerSelection === "rock" && computerSelection === "scissors") {
+    } else if (playerSelection === "ROCK" && computerSelection === "SCISSORS") {
         playerScore += 1;
-        document.getElementById("status").innerHTML = "<p>The computer chose scissors.</p><p><br>You won! Rock beats scissors.</p>";
+        document.getElementById("status").innerHTML = `<p>The computer chose ${computerSelection}.<br><br><br>You won! ${playerSelection} beats ${computerSelection}.<br><br><br>You earned a point!</p>`;
         document.getElementById("player-score").innerHTML = `<br>${playerScore}`;
-    } else if (playerSelection === "paper" && computerSelection === "rock") {
+    } else if (playerSelection === "PAPER" && computerSelection === "ROCK") {
         playerScore += 1;
-        document.getElementById("status").innerHTML = "<p>The computer chose rock.</p><p><br>You won! Paper beats rock.</p>";
+        document.getElementById("status").innerHTML = `<p>The computer chose ${computerSelection}.<br><br><br>You won! ${playerSelection} beats ${computerSelection}.<br><br><br>You earned a point!</p>`;
         document.getElementById("player-score").innerHTML = `<br>${playerScore}`;
-    } else if (playerSelection === "scissors" && computerSelection === "paper") {
+    } else if (playerSelection === "SCISSORS" && computerSelection === "PAPER") {
         playerScore += 1;
-        document.getElementById("status").innerHTML = "<p>The computer chose paper.</p><p><br>You won! Scissors beats paper.</p>";
+        document.getElementById("status").innerHTML = `<p>The computer chose ${computerSelection}.<br><br><br>You won! ${playerSelection} beats ${computerSelection}.<br><br><br>You earned a point!</p>`;
         document.getElementById("player-score").innerHTML = `<br>${playerScore}`;
     } else {
         // let player know if it's a tie. score doesn't change
-        document.getElementById("status").innerHTML = `<p>The computer chose ${computerSelection}.</p><p><br>It's a tie! Let's play again.</p>`;
+        document.getElementById("status").innerHTML = `<p>The computer also chose ${computerSelection}.<br><br><br>Let's re-play this round...</p>`;
     }
 }
