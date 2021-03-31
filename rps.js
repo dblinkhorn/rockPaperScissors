996c32dc98a1bb9d5ac9cc98031a54800b8d0261
dblinkhorn <dblinkhorn@gmail.com>
2021-03-31 00:02:44 -0700

    added play again button on game over status update. tweaked grid layout to accommodate future typewriter text animation for status updates.

--- rps.js
+++ rps.js
@@ -34,28 +34,106 @@ scissors.addEventListener("click", function () {
     playRound(playerSelection);
 });
 
+let playAgain = document.getElementById("status-middle");
+    playAgain.addEventListener("click", function () {
+    document.getElementById("status-top").innerHTML = "<p><center>Human vs machine!</center></p>";
+    document.getElementById("status-middle").innerHTML = "<p><center>First one to 5 points wins.</center></p>";
+    document.getElementById("status-bottom").innerHTML = "<p><center>Select your weapon below.</center></p>";
+    resetScores();
+})
+
+
+// function to perform typewriter text effect on status updates
+
+// variables used function
+// var string1 = "this should be the text of string1";
+// var string2 = "this should be the text of string2";
+// var i = 0;
+// var j = 0;
+// var speed = 20; // delay in miliseconds between each letter being added
+
+// function typeWriter(string, element, counter) {
+//   setTimeout(()=> {
+
+//   if (counter < string.length) {
+//     document.getElementById(element).innerHTML += string.charAt(counter);
+//     counter++;
+//     typeWriter(string, element, counter)
+//   }
+//   }, 20)
+// }
+
+
+// typeWriter(string1, "typewriterDiv1", i);
+// typeWriter(string2, "typewriterDiv2", j);
+
+// variables to use in declareWinner function depending on round winner
+winResult = "You won!"
+loseResult = "You lost..."
+winMessage = "Keep it up!"
+loseMessage = "Better luck next round!"
+
+// function used in main playRound function to avoid lots of repeat code
+function declareWinner(result, message) {
+    document.getElementById("status-top").innerHTML = `<p>The computer chose ${computerSelection}.</p>`;
+    document.getElementById("status-middle").innerHTML = `<p>${result} ${playerSelection} beats ${computerSelection}.</p>`;
+    document.getElementById("status-bottom").innerHTML = `<p>${message}</p>`;
+}
+
+// funtion to award points and change score element on computer win
+function computerWin() {
+    computerScore += 1;
+    declareWinner(loseResult, loseMessage);
+    document.getElementById("computer-score").innerHTML = `<br>${computerScore}`;
+}
+
+// same as above but for player win
+function playerWin() {
+    playerScore += 1;
+    declareWinner(winResult, winMessage);
+    document.getElementById("player-score").innerHTML = `<br>${playerScore}`;
+
+}
+
+
 // function to alert player of result of game once either player reaches 5 points
 function checkWinner(pScore, cScore) {
 
-    function resetScores() {
-        computerScore = 0;
-        document.getElementById("computer-score").innerHTML = `<br>${computerScore}`;
-        playerScore = 0;
-        document.getElementById("player-score").innerHTML = `<br>${playerScore}`;
+    if (pScore === 5) {
+        setTimeout( function() {
+            document.getElementById("status-top").innerHTML = `Your ${playerSelection} beats ${computerSelection}.<br><br>You won the game!!`;
+        }, 1500);
+        setTimeout( function() {
+            document.getElementById("status-middle").innerHTML = "<center><button class='grow' id='play-again'>PLAY AGAIN?</button></center>";
+        }, 1500);
+        setTimeout( function() {
+            document.getElementById("status-bottom").innerHTML = "";
+        }, 1500);
     }
 
-    if (pScore === 5) {
-            setTimeout(function(){document.getElementById("status").innerHTML = "Congratulations, you defeated the computer!<br><br><br>To play again, choose a weapon below."}, 1500);
-            resetScores();
-        }
     if (cScore === 5) {
-            setTimeout(function(){document.getElementById("status").innerHTML = "Sorry, but the computer has defeated you!<br><br><br>To play again, choose a weapon below."}, 1500);
-            resetScores()
-        }
+        setTimeout( function() {
+            document.getElementById("status-top").innerHTML = `The computer's ${computerSelection} beats ${playerSelection}.<br><br>You lost the game...`;
+        }, 1500);
+        setTimeout( function() {
+            document.getElementById("status-middle").innerHTML = "<center><button class='grow' id='play-again'>PLAY AGAIN?</button></center>";
+        }, 1500);
+        setTimeout( function() {
+            document.getElementById("status-bottom").innerHTML = "";
+        }, 1500);
+    }
 
         console.log(`resetScores computer score = ${computerScore}.`);
         console.log(`resetScores player score = ${playerScore}.`);
-    }
+}
+
+// resets all scores to 0 for new game
+function resetScores() {
+    computerScore = 0;
+    document.getElementById("computer-score").innerHTML = `<br>${computerScore}`;
+    playerScore = 0;
+    document.getElementById("player-score").innerHTML = `<br>${playerScore}`;
+}
 
 // function that determines outcome of the round
 function playRound(playerSelection) {
@@ -78,35 +156,22 @@ function playRound(playerSelection) {
 
     // main game logic, determines winner of round based on selections
     if (playerSelection === "ROCK" && computerSelection === "PAPER") {
-        // award point to score
-        computerScore += 1;
-        // update status div with result of the round
-        document.getElementById("status").innerHTML = `<p>The computer chose ${computerSelection}.<br><br><br>You lost... ${computerSelection} beats ${playerSelection}.<br><br><br>Better luck next round!</p>`;
-        // update score
-        document.getElementById("computer-score").innerHTML = `<br>${computerScore}`;
+        computerWin();
     } else if (playerSelection === "PAPER" && computerSelection === "SCISSORS") {
-        computerScore += 1;
-        document.getElementById("status").innerHTML = `<p>The computer chose ${computerSelection}.<br><br><br>You lost... ${computerSelection} beats ${playerSelection}.<br><br><br>Better luck next round!</p>`;
-        document.getElementById("computer-score").innerHTML = `<br>${computerScore}`;
+        computerWin();
     } else if (playerSelection === "SCISSORS" && computerSelection === "ROCK") {
-        computerScore += 1;
-        document.getElementById("status").innerHTML = `<p>The computer chose ${computerSelection}.<br><br><br>You lost... ${computerSelection} beats ${playerSelection}.<br><br><br>Better luck next round!</p>`;
-        document.getElementById("computer-score").innerHTML = `<br>${computerScore}`;
+        computerWin();
     } else if (playerSelection === "ROCK" && computerSelection === "SCISSORS") {
-        playerScore += 1;
-        document.getElementById("status").innerHTML = `<p>The computer chose ${computerSelection}.<br><br><br>You won! ${playerSelection} beats ${computerSelection}.<br><br><br>You earned a point!</p>`;
-        document.getElementById("player-score").innerHTML = `<br>${playerScore}`;
+        playerWin();
     } else if (playerSelection === "PAPER" && computerSelection === "ROCK") {
-        playerScore += 1;
-        document.getElementById("status").innerHTML = `<p>The computer chose ${computerSelection}.<br><br><br>You won! ${playerSelection} beats ${computerSelection}.<br><br><br>You earned a point!</p>`;
-        document.getElementById("player-score").innerHTML = `<br>${playerScore}`;
+        playerWin();
     } else if (playerSelection === "SCISSORS" && computerSelection === "PAPER") {
-        playerScore += 1;
-        document.getElementById("status").innerHTML = `<p>The computer chose ${computerSelection}.<br><br><br>You won! ${playerSelection} beats ${computerSelection}.<br><br><br>You earned a point!</p>`;
-        document.getElementById("player-score").innerHTML = `<br>${playerScore}`;
+        playerWin();
     } else {
         // let player know if it's a tie. score doesn't change
-        document.getElementById("status").innerHTML = `<p>The computer also chose ${computerSelection}.<br><br><br>Let's re-play this round...</p>`;
+        document.getElementById("status-top").innerHTML = `<p>The computer also chose ${computerSelection}.`;
+        document.getElementById("status-middle").innerHTML = `<p>Let's re-play this round...</p>`;
+        document.getElementById("status-bottom").innerHTML = `<p>Choose your weapon below.`;
     }
 
     checkWinner(playerScore, computerScore);
